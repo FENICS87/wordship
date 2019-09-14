cff1f2be2f1ccbbb98be289a1fa75498d121e039
FENICS87 <54675311+FENICS87@users.noreply.github.com>
2019-09-14 19:20:38 +0300

    Update js.js

--- js/js.js
+++ js/js.js
@@ -1,27 +1,28 @@
 "use strict";
 let table = document.querySelector("table"),
     matrix = Array.from(table.querySelectorAll("tr")).slice(1);
-for(let i = 0; i < matrix.length; i++){   //превращаем таблицу в матрицу
-    matrix[i] = matrix[i].cells;
+for(let i = 0; i < matrix.length; i++){
+    matrix[i] = Array.from(matrix[i].cells).slice(1);
+  
 }
-let warshipLocace=[]; //переменная для хранения координат целых кораблей
+let warshipLocace=[];
 
 table.tBodies[0].addEventListener("mousedown", ({target})=>{ 
     let tr = target.closest("tr");
-    if(tr.firstElementChild=== target){ //убираем обработчик с нумеровванного поля
+    if(tr.firstElementChild=== target){
         return
     }
-    if(target.classList.contains("warship")){// проверяем наличия попадания и маркируем
+    if(target.classList.contains("warship")){
         target.classList.add("hurt");
-        itog();
+        zzz();
     }else {
         target.classList.add("shot");}
+    
 });
-
 
  start();
 
-function start(matrix){///1
+function start(matrix){
     let warship4 = [4, 1],   // длина корабля, количество
         warship3 = [3, 2],
         warship2 = [2, 3],
@@ -33,31 +34,23 @@ function start(matrix){///1
    location1(warship1);
 }
 
-function location1(warship){///2 функция по определению координат кораблей
+function location1(warship){
     let i = 0;
     for(let i =0; i<warship[1]; i++){
         while( i < warship[1]){
-            let  warshipDirection = (Math.random() * 10 < 5) ? "row":"column";
-    
+            let  warshipDirection = (Math.random() * 10 > 5) ? "row":"column";
             let coordinateX = (warshipDirection ==="row") ? 
-            Math.ceil(Math.random() * (10-warship[0])):
-            Math.ceil(Math.random() * 10);
+            Math.floor(Math.random() * (matrix[0].length - warship[0])):
+            Math.floor(Math.random() * 10);
 
-            let coordinateY = (warshipDirection ==="row") ? 
-            Math.floor(Math.random() * 10):
-            Math.floor(Math.random() * (10-warship[0]));
+            let coordinateY = (warshipDirection === "row") ? 
+            Math.floor(Math.random() * matrix.length):
+            Math.floor(Math.random() * (matrix.length - warship[0]));
               
-            if(matrix[coordinateY][coordinateX].classList.contains("warship") || matrix[coordinateY][coordinateX].classList.contains("warshipClosed")){
-                continue;//проверяем на соблюдение условий растановки. А есть метод для проверки нескольких классов????????
-            }
-            if(warshipDirection==="row"){ 
-                if(matrix[coordinateY][coordinateX + warship[0]].classList.contains("warship") || matrix[coordinateY][coordinateX + warship[0]].classList.contains("warshipClosed")){
-                    continue;
-                }        
-            }else{
-                if(matrix[coordinateY + warship[0]][coordinateX].classList.contains("warship") || matrix[coordinateY + warship[0]][coordinateX].classList.contains("warshipClosed")){
-                    continue; 
-                }
+            if(matrix[coordinateY][coordinateX].hasAttribute('class')||
+            (warshipDirection==="row")&&(matrix[coordinateY][coordinateX + warship[0]].hasAttribute('class'))||
+            (warshipDirection==="column")&&(matrix[coordinateY + warship[0]][coordinateX].hasAttribute('class'))){
+                continue;
             }
             installation( coordinateX,  coordinateY, warshipDirection, warship);
         i++;
@@ -65,80 +58,60 @@ function location1(warship){///2 функция по определению ко
     }
 }
 
-function installation( coordinateX,  coordinateY, warshipDirection, warship){///3  функция по растановке кораблей
-    let warshipLocace1=[];////////////
+function installation( coordinateX,  coordinateY, warshipDirection, warship){
+    let warshipLocace1=[];
     if(warshipDirection === "row") {
-        for(let i = coordinateX; i <  coordinateX + warship[0]; i++){
-            matrix[coordinateY][i].classList.add("warship");
-            warshipLocace1.push( matrix[coordinateY][i]);
-            if(coordinateY < 9){                                  //помечаем смежные клетки с кораблями
-                matrix[coordinateY+1][i].classList.add("warshipClosed");
+        for(let x = coordinateX - 1; x <=  coordinateX + warship[0]; x++){
+           if(!((x === coordinateX - 1)||(x ===  coordinateX + warship[0]))){
+               matrix[coordinateY][x].classList.add("warship");
+                warshipLocace1.push( matrix[coordinateY][x]);
             }
-            if(coordinateY > 0){
-                matrix[coordinateY-1][i].classList.add("warshipClosed");
+           
+            if((x < 0)||(x === matrix[0].length)){
+                continue;
             }
-            if( (i === coordinateX) && !(coordinateX===1) ){
-                if(coordinateY >0){
-                    matrix[coordinateY-1][i-1].classList.add("warshipClosed");
+            for(let y = coordinateY - 1; y < coordinateY + 2; y++){
+                if((y<0)||(y === matrix.length)){
+                    continue;
                 }
-                matrix[coordinateY][i-1].classList.add("warshipClosed");
-                if(coordinateY < 9){
-                    matrix[coordinateY+1][i-1].classList.add("warshipClosed");
-                }
-            }
-            if( (i === coordinateX + warship[0] - 1) && !(i===10) ){
-                if(coordinateY >0){
-                    matrix[coordinateY-1][i+1].classList.add("warshipClosed");
-                }
-                matrix[coordinateY][i+1].classList.add("warshipClosed");
-                if(coordinateY < 9){
-                    matrix[coordinateY+1][i+1].classList.add("warshipClosed");
+                if(matrix[y][x].classList.contains("warship")){
+                    continue;
+                }else{
+                    matrix[y][x].classList.add("warshipClosed"); 
                 }
             }
         }
-    }       
-    else{
-        for(let i = coordinateY; i < coordinateY + warship[0]; i++){
-            matrix[i][coordinateX].classList.add("warship");
-            warshipLocace1.push( matrix[i][coordinateX]);////////
-            if(coordinateX > 1){
-                matrix[i][coordinateX-1].classList.add("warshipClosed");
+    }else{
+        for(let y = coordinateY - 1; y <=  coordinateY + warship[0]; y++){
+           if(!((y === coordinateY - 1)||(y ===  coordinateY + warship[0]))){
+               matrix[y][coordinateX].classList.add("warship");
+               warshipLocace1.push( matrix[y][coordinateX]);/////////////////////////////////!!!!!!!!!!!!!!!!!!!
             }
-            if(coordinateX < 10){
-                matrix[i][coordinateX+1].classList.add("warshipClosed");
+            
+            if((y < 0)||(y === matrix.length)){
+                continue;
             }
-            if( (i === coordinateY) && !(coordinateY===0) ){
-                if(coordinateX >1){
-                    matrix[i-1][coordinateX-1].classList.add("warshipClosed");
+            for(let x = coordinateX - 1; x < coordinateX + 2; x++){
+                if((x<0)||(x === matrix.length)){
+                    continue;
                 }
-                matrix[i-1][coordinateX].classList.add("warshipClosed");
-                if(coordinateX < 10){
-                    matrix[i-1][coordinateX+1].classList.add("warshipClosed");
-                }
-            }
-            if( (i === coordinateY + warship[0] - 1) && !(i===11) ){
-                if(coordinateX >1){
-                    matrix[i+1][coordinateX-1].classList.add("warshipClosed");
-                }
-                matrix[i+1][coordinateX].classList.add("warshipClosed");
-                if(coordinateX < 10){
-                    matrix[i+1][coordinateX+1].classList.add("warshipClosed");
+                if(matrix[y][x].classList.contains("warship")){
+                    continue;
+                }else{
+                    matrix[y][x].classList.add("warshipClosed"); 
                 }
             }
-        }    
+        }
     }
     warshipLocace.push(warshipLocace1);
- 
-    
 }
-function itog(){// функция отмечает уничтоженные корабли
+function zzz(){
     for(let i=0; i<warshipLocace.length; i++){
         let caunter=0; 
         for(let j =0; j<warshipLocace[i].length; j++){
             
            if(warshipLocace[i][j].classList.contains("hurt")){
                caunter++;
-            
                if(caunter===warshipLocace[i].length) {
                 for(let z = 0; z < warshipLocace[i].length; z++){ 
                     warshipLocace[i][z].classList.add("smite");
@@ -155,6 +128,7 @@ function itog(){// функция отмечает уничтоженные ко
                   warshipLocace.splice(i, 1);   
                }
             }
-        } 
+        }
     }
 }
+
